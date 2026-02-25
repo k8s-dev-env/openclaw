a71c94ac06374b0b7e6dc56a1ec61fcb8dfec048
Maxime <machanwei@gmail.com>
2026-02-25 06:11:48 +0000

    chore: Update start.sh to mount SSH directory and install jq along with chromium

--- start.sh
+++ start.sh
@@ -7,14 +7,15 @@ docker run -d \
 --env-file openclaw.env \
 --env-file .env \
 -v ./openclaw-data:/home/node/.openclaw \
+-v ./.ssh:/home/node/.ssh \
 --name openclaw \
 -p 18789:18789 \
 --shm-size=2gb \
 --cap-add=SYS_ADMIN \
-ghcr.io/openclaw/openclaw 
+ghcr.io/openclaw/openclaw:latest
 # r82wei/openclaw:latest
 
-docker exec -u 0 openclaw bash -c 'apt update && apt install -y chromium'
+docker exec -u 0 openclaw bash -c 'apt update && apt install -y jq chromium'
 docker exec -u 0 openclaw bash -c 'nohup /usr/bin/chromium \
   --headless=new \
   --no-sandbox \
