1b2acb9ede27f55792164025f622c95280c7e79a
Maxime <machanwei@gmail.com>
2026-02-27 19:17:14 +0800

    chore: upgrade openclaw version to v2026.2.25

--- Dockerfiles/webtop-openclaw/build.sh
+++ Dockerfiles/webtop-openclaw/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 WEBTOP_VERSION=ubuntu-kde-version-df170c95
-OPENCLAW_VERSION=v2026.2.24
+OPENCLAW_VERSION=v2026.2.25
 
 docker buildx build --load --no-cache --build-arg WEBTOP_VERSION=${WEBTOP_VERSION} --build-arg OPENCLAW_VERSION=${OPENCLAW_VERSION} -t r82wei/webtop-openclaw:${WEBTOP_VERSION}-${OPENCLAW_VERSION} .
--- Dockerfiles/webtop-openclaw/release.sh
+++ Dockerfiles/webtop-openclaw/release.sh
@@ -3,5 +3,5 @@
 WEBTOP_VERSION=ubuntu-kde-version-df170c95
 OPENCLAW_VERSION=v2026.2.25
 
-docker buildx build --no-cache --platform linux/amd64,linux/arm64 --push  -t r82wei/webtop-openclaw:${WEBTOP_VERSION}-${OPENCLAW_VERSION} .
-docker buildx build --platform linux/amd64,linux/arm64 --push  -t r82wei/webtop-openclaw:latest .
+docker buildx build --no-cache --platform linux/amd64,linux/arm64 --push --build-arg WEBTOP_VERSION=${WEBTOP_VERSION} --build-arg OPENCLAW_VERSION=${OPENCLAW_VERSION} -t r82wei/webtop-openclaw:${WEBTOP_VERSION}-${OPENCLAW_VERSION} .
+docker buildx build --platform linux/amd64,linux/arm64 --push --build-arg WEBTOP_VERSION=${WEBTOP_VERSION} --build-arg OPENCLAW_VERSION=${OPENCLAW_VERSION} -t r82wei/webtop-openclaw:latest .
