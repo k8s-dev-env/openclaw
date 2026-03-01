7dfb2b16f2ada11a85f1dccabb43d3cd872b2842
Maxime <machanwei@gmail.com>
2026-03-01 06:39:29 +0000

    feat: Enhance backup script to include OpenClaw ID in backup filenames and remove temporary tar files

--- backup.sh
+++ backup.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+source .env
+
 # get the date with timezone Asia/Taipei (+8)
 DATE=$(TZ=Asia/Taipei date +%Y%m%d-%H%M%S)
 echo DATE: $DATE
 
 # backup the .openclaw directory、.env、.ssh
-sudo tar -zcvf openclaw-data-$DATE.tar.gz .openclaw webtop .env 
-zip -e openclaw-data-$DATE.zip openclaw-data-$DATE.tar.gz
-echo "openclaw-data backup saved to openclaw-data-$DATE.zip"
+sudo tar -zcvf openclaw-data.${OPENCLAW_ID}.$DATE.tar.gz .openclaw webtop .env 
+zip -e openclaw-data.${OPENCLAW_ID}.$DATE.zip openclaw-data.${OPENCLAW_ID}.$DATE.tar.gz
+echo "OpenClaw data backup saved to openclaw-data.${OPENCLAW_ID}.$DATE.zip"
+rm -rf openclaw-data.${OPENCLAW_ID}.$DATE.tar.gz
